74819e86e5294d2c354dcde2de31669f3809866d
naya2016 <63491927+naya2016@users.noreply.github.com>
2023-05-02 16:49:35 +0800

    Update hello.php

--- api/hello.php
+++ api/hello.php
@@ -2,9 +2,9 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = mt_rand(0,98);
+$num = mt_rand(0,3);
 //拼接图片地址
-$picpath = "https://cdn.jsdelivr.net/gh/UniqueYou/wallpaper-transverse@main/".$num.".jpg";
+$picpath = "https://cdn.jsdelivr.net/gh/naya2016/cdn@main/".$num.".gif";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
