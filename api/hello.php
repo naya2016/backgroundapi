8b825bb1fe9fd274b8cebf89c4c2441256eac6a9
naya2016 <63491927+naya2016@users.noreply.github.com>
2024-04-03 18:59:28 +0800

    Update hello.php

--- api/hello.php
+++ api/hello.php
@@ -2,7 +2,7 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = mt_rand(0,5);
+$num = mt_rand(5,9);
 //拼接图片地址
 $picpath = "https://github.moeyy.xyz/https://github.com/naya2016/cdn/releases/download/cdn/".$num.".jpg";
 //重定位到图片
