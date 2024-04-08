49604700a729426e6e96af93ea55f61908421409
naya2016 <63491927+naya2016@users.noreply.github.com>
2024-04-08 18:40:47 +0800

    切换数字 hello.php

--- api/hello.php
+++ api/hello.php
@@ -2,7 +2,7 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = mt_rand(5,9);
+$num = mt_rand(1,4);
 //拼接图片地址
 $picpath = "https://github.moeyy.xyz/https://github.com/naya2016/cdn/releases/download/cdn/".$num.".jpg";
 //重定位到图片
